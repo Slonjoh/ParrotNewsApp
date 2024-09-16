71c7f1e94953b616a66786e1e448dbb6759f8eb2
Slonjoh <tobson11122@gmail.com>
2024-09-16 14:05:45 -0400

    Final home screen touch

--- navigation/StoryCard.js
+++ navigation/StoryCard.js
@@ -1,24 +1,34 @@
-import React, { memo } from 'react';
+import React, { Component } from 'react';
 import { Card, Title, Paragraph } from 'react-native-paper';
 
-// Memoized StoryCard Component
-const StoryCard = memo(({ item, openStory, isRead }) => (
-  <Card
-    style={{
-      margin: 10,
-      backgroundColor: isRead ? '#e0e0e0' : '#fff', // Change background color based on read status
-    }}
-    onPress={item.url ? () => openStory(item.url, item.id) : null} // Only set onPress if there's a URL
-  >
-    <Card.Content>
-      <Title>{item.title}</Title>
-      <Paragraph>By {item.by}</Paragraph>
-      <Paragraph>Score: {item.score}</Paragraph>
-    </Card.Content>
-  </Card>
-), (prevProps, nextProps) => {
-  // Only re-render if `isRead` or `item` changes
-  return prevProps.isRead === nextProps.isRead && prevProps.item.id === nextProps.item.id;
-});
+class StoryCard extends Component {
+  shouldComponentUpdate(nextProps) {
+    // Update only if `isRead` or `item.id` changes
+    return (
+      nextProps.isRead !== this.props.isRead ||
+      nextProps.item.id !== this.props.item.id
+    );
+  }
+
+  render() {
+    const { item, openStory, isRead } = this.props;
+
+    return (
+      <Card
+        style={{
+          margin: 10,
+          backgroundColor: isRead ? '#f0f0f0' : '#fff', // Change background color based on read status
+        }}
+        onPress={item.url ? () => openStory(item.url, item.id) : null} // Only set onPress if there's a URL
+      >
+        <Card.Content>
+          <Title style={{ fontSize: 18, color: isRead ? '#555' : '#000' }} allowFontScaling={false}>{item.title}</Title>
+          <Paragraph style={{ fontSize: 14, color: isRead ? '#777' : '#333' }} allowFontScaling={false}>By {item.by}</Paragraph>
+          <Paragraph style={{ fontSize: 14, color: isRead ? '#777' : '#333' }} allowFontScaling={false}>Score: {item.score}</Paragraph>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
 export default StoryCard;
